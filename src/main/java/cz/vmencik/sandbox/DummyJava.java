624372ac8da7b0906eae4be438065cb690bd5076
Vlastimil Mencik <v_mencik@hotmail.com>
2012-06-01 10:40:12 +0200

    Scala/Java cross compilation by Gradle

--- src/main/java/cz/vmencik/sandbox/DummyJava.java
+++ src/main/java/cz/vmencik/sandbox/DummyJava.java
@@ -3,6 +3,6 @@ package cz.vmencik.sandbox;
 public class DummyJava {
 
 	public String say() {
-		return "With Java and Scala classes mixed together";
+		return Dummy.text() ;
 	}
 }
--- src/main/scala/cz/vmencik/sandbox/Dummy.scala
+++ src/main/scala/cz/vmencik/sandbox/Dummy.scala
@@ -3,4 +3,6 @@ package cz.vmencik.sandbox
 object Dummy extends App {
   println("This is just an example project")
   println(new DummyJava().say())
+  
+  def text() = "With Java and Scala classes mixed together, cross-compiled by Scala compiler"
 }
